0df8a7313a469fbb39b3b1d68d1cfa8ba14abdef
Jean-Baptiste Astruc <jeanbaptisteastruc@gmail.com>
2025-02-04 13:24:51 +0100

    Small adjustments after forge build warnings

--- src/Coinflip.sol
+++ src/Coinflip.sol
@@ -12,24 +12,24 @@ contract Coinflip is Ownable{
     // An instance of the random number requestor, client interface
     DirectFundingConsumer private vrfRequestor;
 
-    ///@dev we no longer use the seed, instead each coinflip deployment should spawn its own VRF instance so that the Coinflip smart contract is the owner of the DirectFunding contract.
-    ///@notice This programming pattern is known as a factory model - a contract creating other contracts 
+    /// @dev we no longer use the seed, instead each coinflip deployment should spawn its own VRF instance so that the Coinflip smart contract is the owner of the DirectFunding contract.
+    /// @notice This programming pattern is known as a factory model - a contract creating other contracts 
     constructor() Ownable(msg.sender) {}
 
-    ///@notice Fund the VRF instance with **5** LINK tokens.
-    ///@return boolean of whether funding the VRF instance with link tokens was successful or not
-    ///@dev use the address of LINK token contract provided. Do not change the address!
-    ///@custom: Attention! In order for this contract to fund another contract, which tokens does this contract need to have before calling this function? What **additional** functions does this contract need to "receive" these tokens itself?
+    /// @notice Fund the VRF instance with **5** LINK tokens.
+    /// @return boolean of whether funding the VRF instance with link tokens was successful or not
+    /// @dev use the address of LINK token contract provided. Do not change the address!
+    /// @custom:important Attention! In order for this contract to fund another contract, which tokens does this contract need to have before calling this function? What **additional** functions does this contract need to "receive" these tokens itself?
     function fundOracle() external returns(bool){
         address Link_addr = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
     }
 
-    ///@notice user guess THREE flips either a 1 or a 0.
-    ///@param uint8 3 guesses - which is "required" to be 1 or 0
-    ///@dev After validating the user input, store the user input and request ID in their respective global mappings and call the "requestRandomWords" function in VRF instance
-    ///@custom: Attention! How do we make sure 3 random numbers are requested?
-    ///@dev Then, store the requestid in global mapping
-    function userInput(uint8[3] Guesses) external {}
+    /// @notice user guess THREE flips either a 1 or a 0.
+    /// @param Guesses 3 coinflip guesses - which is "required" to be 1 or 0
+    /// @dev After validating the user input, store the user input and request ID in their respective global mappings and call the "requestRandomWords" function in VRF instance
+    /// @custom:important Attention! How do we make sure 3 random numbers are requested?
+    /// @dev Then, store the requestid in global mapping
+    function userInput(uint8[3] calldata Guesses) external {}
 
     ///@notice Due to the fact that a blockchain does not deliver data instantaneously, in fact quite slowly under congestion, allow users to check the status of their request.
     ///@return boolean of whether the request has been fulfilled or not
